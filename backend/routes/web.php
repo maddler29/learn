1d61b160f999c37a41777dff051c59fb61d22a19
DESKTOP-3R4UBLV\MADLAX <menta.masa29.php@gmail.com>
2021-06-08 23:16:58 +0900

    配列を中心に勉強しました。paizaは標準入力が多いのでここには書いてないですけど勉強はしております。(変数に標準入力の値をすべていれて書くという方法も考えています)

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-
     $student = ["田中","井伊","織田"];
     dd($student);
 });
+
+
+Route::get('/learn01', 'App\Http\Controllers\LearnController@index')->name('learn.index');
+
+
